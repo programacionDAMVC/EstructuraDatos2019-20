4a996b699d70d5c3899c5422e7ccacea0a235102
manuel molino <manolo@iesvirgendelcarmen.com>
2019-11-13 11:08:51 +0100

    modificado:     Columnas.java (controlamos el nº de columnas y el propio número)

--- Columnas.java
+++ Columnas.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Columnas {
 	
 	public static void main (String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce el nº de columnas");
-		int numeroColumnas = sc.nextInt();
-		System.out.println("Introduce el nº a imprimir");
-		int numero = sc.nextInt();
+		//controlamos que el nº de columnas sea distinto de cero y positivo
+		//controlamos que el número este comprendido entre 1 y 1000 (inclusive)
+		int numeroColumnas = 0;
+		int numero = 0;
+		do {
+			
+			System.out.println("Introduce el nº de columnas (máximo 10)");
+			numeroColumnas = sc.nextInt();
+			System.out.println("Introduce el nº a imprimir (superior a 1 e inferior a 1000)");
+			numero = sc.nextInt();
+			
+		} while ( numeroColumnas == 0 || numeroColumnas < 0 || numeroColumnas > 10
+					|| numero < 1 || numero > 1000);
 		sc.close();
 		
 		System.out.println("Imprimiendo nº en columnas");
